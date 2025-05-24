911480c30c23b36f06e819c545f19f48b25520f1
gloria <gloriaozioma586@gmail.com>
2025-05-24 05:06:02 +0100

    Add simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000 # investment
+rate = 0.05 # 5%  interest rate on investment
+time = 3 # 3 years duration of investment
+interest = principal* rate * time # formular for component
+print("The simple interest is: ,150.0")
